d28ed4c60742d2ed6b000e2074e476f170f3a9e3
king-king <niuyiwangqun@sohu.com>
2018-01-19 13:15:49 +0800

    change

--- src/react/2.5.2_0/build/background.js
+++ src/react/2.5.2_0/build/background.js
@@ -1,4 +1,4 @@
-!function(modules) {
+! function (modules) {
     function __webpack_require__(moduleId) {
         if (installedModules[moduleId]) return installedModules[moduleId].exports;
         var module = installedModules[moduleId] = {
@@ -6,36 +6,42 @@
             id: moduleId,
             loaded: !1
         };
-        return modules[moduleId].call(module.exports, module, module.exports, __webpack_require__), 
-        module.loaded = !0, module.exports;
+        return modules[moduleId].call(module.exports, module, module.exports, __webpack_require__),
+            module.loaded = !0, module.exports;
     }
     var installedModules = {};
-    return __webpack_require__.m = modules, __webpack_require__.c = installedModules, 
-    __webpack_require__.p = "", __webpack_require__(0);
-}([ function(module, exports) {
+    return __webpack_require__.m = modules, __webpack_require__.c = installedModules,
+        __webpack_require__.p = "", __webpack_require__(0);
+}([function (module, exports) {
     "use strict";
+
     function isNumeric(str) {
         return +str + "" === str;
     }
+
     function installContentScript(tabId) {
         chrome.tabs.executeScript(tabId, {
             file: "/build/contentScript.js"
-        }, function() {});
+        }, function () {});
     }
+
     function doublePipe(one, two) {
         function lOne(message) {
             two.postMessage(message);
         }
+
         function lTwo(message) {
             one.postMessage(message);
         }
+
         function shutdown() {
-            one.onMessage.removeListener(lOne), two.onMessage.removeListener(lTwo), one.disconnect(), 
-            two.disconnect();
+            one.onMessage.removeListener(lOne), two.onMessage.removeListener(lTwo), one.disconnect(),
+                two.disconnect();
         }
-        one.onMessage.addListener(lOne), two.onMessage.addListener(lTwo), one.onDisconnect.addListener(shutdown), 
-        two.onDisconnect.addListener(shutdown);
+        one.onMessage.addListener(lOne), two.onMessage.addListener(lTwo), one.onDisconnect.addListener(shutdown),
+            two.onDisconnect.addListener(shutdown);
     }
+
     function setIconAndPopup(reactBuildType, tabId) {
         chrome.browserAction.setIcon({
             tabId: tabId,
@@ -50,21 +56,23 @@
             popup: "popups/" + reactBuildType + ".html"
         });
     }
-    var ports = {}, IS_FIREFOX = navigator.userAgent.indexOf("Firefox") >= 0;
-    chrome.runtime.onConnect.addListener(function(port) {
-        var tab = null, name = null;
-        isNumeric(port.name) ? (tab = port.name, name = "devtools", installContentScript(+port.name)) : (tab = port.sender.tab.id, 
-        name = "content-script"), ports[tab] || (ports[tab] = {
+    var ports = {},
+        IS_FIREFOX = navigator.userAgent.indexOf("Firefox") >= 0;
+    chrome.runtime.onConnect.addListener(function (port) {
+        var tab = null,
+            name = null;
+        isNumeric(port.name) ? (tab = port.name, name = "devtools", installContentScript(+port.name)) : (tab = port.sender.tab.id,
+            name = "content-script"), ports[tab] || (ports[tab] = {
             devtools: null,
             "content-script": null
         }), ports[tab][name] = port, ports[tab].devtools && ports[tab]["content-script"] && doublePipe(ports[tab].devtools, ports[tab]["content-script"]);
-    }), IS_FIREFOX && chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    }), IS_FIREFOX && chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         tab.active && "loading" === changeInfo.status && setIconAndPopup("disabled", tabId);
-    }), chrome.runtime.onMessage.addListener(function(req, sender) {
+    }), chrome.runtime.onMessage.addListener(function (req, sender) {
         if (req.hasDetectedReact && sender.tab) {
             var reactBuildType = req.reactBuildType;
-            sender.url.indexOf("facebook.github.io/react") !== -1 && (reactBuildType = "production"), 
-            setIconAndPopup(reactBuildType, sender.tab.id);
+            sender.url.indexOf("facebook.github.io/react") !== -1 && (reactBuildType = "production"),
+                setIconAndPopup(reactBuildType, sender.tab.id);
         }
     });
-} ]);
+}]);
